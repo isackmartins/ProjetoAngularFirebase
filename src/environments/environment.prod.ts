4ee23d4ca8b48050276d10af63ed4af39518f959
Ícaro Redini Jaques Eid <141058011+eidicaro@users.noreply.github.com>
2025-02-19 19:26:28 +0000

    criação do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAgqpnXNxZ8abif8ssQq_C2s998w7jZb68",
-    authDomain: "api-aula-d6451.firebaseapp.com",
-    projectId: "api-aula-d6451",
-    storageBucket: "api-aula-d6451.appspot.com",
-    messagingSenderId: "48648388488",
-    appId: "1:48648388488:web:dc73e0a1496a52cd4b843d",
-    measurementId: "G-CR3MDWMWJF"
+    apiKey: "AIzaSyDmiIzbu8NNmP8xej92A6JALyruJaqgXiY",
+  authDomain: "aula-4482a.firebaseapp.com",
+  projectId: "aula-4482a",
+  storageBucket: "aula-4482a.firebasestorage.app",
+  messagingSenderId: "978116038202",
+  appId: "1:978116038202:web:883651015f470953816f03",
+  measurementId: "G-CCX7C6GFXJ"
   },
   production: true
 };
