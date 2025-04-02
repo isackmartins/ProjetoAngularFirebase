38a9e29bcc6897b414ce331b3662a971a662051f
isackmartins <141058061+isackmartins@users.noreply.github.com>
2025-04-02 17:43:54 +0000

    slc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
